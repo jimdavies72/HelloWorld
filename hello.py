71bfb5b74bee9f3786fcaeb2e1d08c0f2899bfba
jimdavies72 <60436624+jimdavies72@users.noreply.github.com>
2020-01-29 16:21:29 +0000

    test commit

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-print('Hello World')
-print('What a beautiful day')
+
+
+def print_hello(text):
+    print(text)
+
+
+#print('Hello World')
+#print('What a beautiful day')
+print_hello('this is a test')
